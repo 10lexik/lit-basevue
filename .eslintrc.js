bd6407ac7ba975d16e124c282a13b5d1022a7fe1
Gwennael Larcher <glarcher@veepee.com>
2024-02-20 11:03:14 +0100

    adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'space-before-function-paren': ['error', 'never'],
+        'indent': ['error', 4],
+        'prefer-template': 'warn'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+}
